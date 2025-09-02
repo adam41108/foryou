51ffe99ca682eabaa1b93e839651ff7a13928e37
zennos <zaly751622@gmail.com>
2025-09-02 11:53:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\nI tried. I cared. But the universe just never gave ME enough space to become “us.”\nI gave you everything — all my effort, all my love — but in the end, you still didn’t choose me.\n You kept choosing someone else, while I was just the person you turned to when you were bored, the one you used to pour out your feelings to… and honestly, I’m just so tired.\nTq and goodbye.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\nI tried. I cared. But the universe just never gave ME enough space to become “us.”\nI gave you everything  all my effort, all my love  but in the end, you still didn’t choose me.\n You kept choosing someone else, while I was just the person you turned to when you were bored, the one you used to pour out your feelings to… and honestly, I’m just so tired.\nTq and goodbye.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -22,3 +22,4 @@ function showLetter() {
   }, 600);
 
 }
+
